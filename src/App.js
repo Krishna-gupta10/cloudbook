5e221fc8d36485eda03e39f34168c9601cb03c35
WoKrishhnaHai <krishnagupta4371@gmail.com>
2023-09-25 22:56:46 +0530

    Initialized React App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome To Cloudbook - Notebook On the Go!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar title="Cloudbook" link1="About" />
+        <h1 className="my-3">Welcome To Cloudbook - Notes On the Go!</h1>
+      </div>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
