216f5b97708befe86bca2ba00813399de30692b0
WoKrishhnaHai <krishnagupta4371@gmail.com>
2023-09-30 03:02:55 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ connectToMongo();
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
